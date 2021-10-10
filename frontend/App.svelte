cf1b8016cc058292ad258e2061bd06cf79959972
Jack Bass <bassj@debian-BULLSEYE-live-builder-AMD64>
2021-10-10 16:59:36 -0400

    Initial Commit

---
+++ frontend/App.svelte
@@ -0,0 +1,32 @@
+<style>
+	main {
+		background: #efefef;
+        height: 100%;
+        width: 100%;
+	}
+
+    .card {
+        background-color: white;
+        display: block;
+        position: relative;
+        top: 100px;
+        left: 100px;
+        width: 100px;
+        height: 100px;
+        border-radius: 6px;
+
+        box-shadow: 0 1px 1px rgba(0,0,0,0.12), 
+              0 2px 2px rgba(0,0,0,0.12), 
+              0 4px 4px rgba(0,0,0,0.12), 
+              0 8px 8px rgba(0,0,0,0.12),
+              0 16px 16px rgba(0,0,0,0.12);
+    }
+</style>
+
+<main>
+
+    <div class="card">
+
+    </div>
+
+</main>
